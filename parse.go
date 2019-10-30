5829eb0d3b09fd49600bea95f052bbbe7a417983
Erik Unger <erik@erikunger.com>
2019-10-30 14:25:36 +0100

    change EnumPackageGoFiles to only enum filePath

--- parse.go
+++ parse.go
@@ -6,16 +6,12 @@ import (
 	"github.com/ungerik/go-fs"
 )
 
-// EnumPackageGoFiles reads all Go files of a package and returns their name and content via callback.
-func EnumPackageGoFiles(packageDir string, callback func(fileName string, fileContent []byte) error) error {
+// EnumPackageGoFiles enums the path of all Go files of a package via callback.
+func EnumPackageGoFiles(packageDir string, callback func(filePath string) error) error {
 	return fs.File(packageDir).ListDir(func(file fs.File) error {
 		if file.IsDir() || file.Ext() != ".go" || strings.HasSuffix(file.Name(), "_test.go") {
 			return nil
 		}
-		fileContent, err := file.ReadAll()
-		if err != nil {
-			return err
-		}
-		return callback(file.Name(), fileContent)
+		return callback(file.LocalPath())
 	})
 }
