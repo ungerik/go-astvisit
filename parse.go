e572dbde4ab499ff64d5f3178ea1454223a662a3
Erik Unger <erik@erikunger.com>
2021-10-08 17:27:06 +0200

    added ParsePackage

--- parse.go
+++ parse.go
@@ -1,11 +1,13 @@
 package astvisit
 
 import (
+	"errors"
 	"fmt"
 	"go/ast"
 	"go/build"
 	"go/parser"
 	"go/token"
+	"io/fs"
 	"path/filepath"
 	"strings"
 
@@ -14,17 +16,17 @@ import (
 
 // ParseDeclarations either from a complete source file
 // or from a code snippet without a package clause.
-func ParseDeclarations(fset *token.FileSet, src string) ([]ast.Decl, error) {
+func ParseDeclarations(fset *token.FileSet, src string) ([]ast.Decl, []*ast.CommentGroup, error) {
 	// Try as whole source file.
 	file, err := parser.ParseFile(fset, "", src, parser.ParseComments)
 	if err == nil {
-		return file.Decls, nil
+		return file.Decls, file.Comments, nil
 	}
 	// If the error is that the source file didn't begin with a
 	// package line and source fragments are ok, fall through to
 	// try as a source fragment. Stop and return on any other error.
 	if !strings.Contains(err.Error(), "expected 'package'") {
-		return nil, err
+		return nil, nil, err
 	}
 
 	// If this is a declaration list, make it a source file
@@ -34,23 +36,47 @@ func ParseDeclarations(fset *token.FileSet, src string) ([]ast.Decl, error) {
 	psrc := "package p; " + src
 	file, err = parser.ParseFile(fset, "", psrc, parser.ParseComments)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return file.Decls, nil
+	return file.Decls, file.Comments, nil
 }
 
 // ParseStatements a from a code snippet
-func ParseStatements(fset *token.FileSet, src string) ([]ast.Stmt, error) {
+func ParseStatements(fset *token.FileSet, src string) ([]ast.Stmt, []*ast.CommentGroup, error) {
 	fsrc := "package p; func _() { " + src + "\n\n}"
 	file, err := parser.ParseFile(fset, "", fsrc, parser.ParseComments)
 	if err != nil {
+		return nil, nil, err
+	}
+	return file.Decls[0].(*ast.FuncDecl).Body.List, file.Comments, nil
+}
+
+var ErrPackageNotFound = errors.New("package not found")
+
+// ParsePackage parses the package source in pkgDir including comments.
+// If filter != nil, only the files with fs.FileInfo entries passing through
+// the filter (and ending in ".go") are considered.
+// Returns a wrapped ErrPackageNotFound error if the filtered pkgDir
+// does not contain Go source for a package.
+func ParsePackage(fset *token.FileSet, pkgDir string, filter func(fs.FileInfo) bool) (pkg *ast.Package, err error) {
+	pkgs, err := parser.ParseDir(fset, pkgDir, filter, parser.ParseComments)
+	if err != nil {
 		return nil, err
 	}
-	return file.Decls[0].(*ast.FuncDecl).Body.List, nil
+	if len(pkgs) == 0 {
+		return nil, fmt.Errorf("%w in %s", ErrPackageNotFound, pkgDir)
+	}
+	if len(pkgs) > 1 {
+		return nil, fmt.Errorf("%d packages found in %s", len(pkgs), pkgDir)
+	}
+	for _, pkg = range pkgs {
+		return pkg, nil
+	}
+	panic("never reached")
 }
 
 type PackageLocation struct {
-	Package    string
+	PkgName    string
 	SourcePath string
 	Std        bool
 }
@@ -71,7 +97,7 @@ func LocatePackage(projectDir, importPath string) (*PackageLocation, error) {
 		return nil, fmt.Errorf("could not load importPath %q for projectDir %q", importPath, projectDir)
 	}
 	return &PackageLocation{
-		Package:    pkgs[0].Name,
+		PkgName:    pkgs[0].Name,
 		SourcePath: filepath.Dir(pkgs[0].GoFiles[0]),
 		Std:        strings.HasPrefix(pkgs[0].GoFiles[0], build.Default.GOROOT),
 	}, nil
@@ -92,7 +118,7 @@ func ImportLineInfo(projectDir, importLine string) (importName string, loc *Pack
 	}
 	importName = strings.TrimSpace(importLine[:begQuote])
 	if importName == "" {
-		importName = loc.Package
+		importName = loc.PkgName
 	}
 	return importName, loc, nil
 }
@@ -104,34 +130,7 @@ func ImportSpecInfo(projectDir string, importSpec *ast.ImportSpec) (importName s
 	}
 	importName = ExprString(importSpec.Name)
 	if importName == "" {
-		importName = loc.Package
+		importName = loc.PkgName
 	}
 	return importName, loc, nil
 }
-
-// // EnumPackageGoFiles enums the path of all non test .go files of a package via callback.
-// func EnumPackageGoFiles(pkgDir string, fset *token.FileSet, callback func(fset *token.FileSet, filePath string) error) error {
-// 	return fs.File(pkgDir).ListDir(func(file fs.File) error {
-// 		if file.IsDir() || file.Ext() != ".go" || strings.HasSuffix(file.Name(), "_test.go") {
-// 			return nil
-// 		}
-// 		return callback(fset, file.LocalPath())
-// 	})
-// }
-
-// // EnumPackageGoFilesRecursive enums the path of all non test .go files of a package
-// // and all sub-packages (basePkgDir/...) via callback.
-// // For every package new token.FileSet is created and passed to the callback.
-// func EnumPackageGoFilesRecursive(basePkgDir string, callback func(fset *token.FileSet, filePath string) error) error {
-// 	fset := token.NewFileSet()
-// 	return fs.File(basePkgDir).ListDir(func(file fs.File) error {
-// 		if file.IsDir() {
-// 			return EnumPackageGoFilesRecursive(file.LocalPath(), callback)
-// 		}
-
-// 		if file.Ext() != ".go" || strings.HasSuffix(file.Name(), "_test.go") {
-// 			return nil
-// 		}
-// 		return callback(fset, file.LocalPath())
-// 	})
-// }
