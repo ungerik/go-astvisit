c164ddf97da11688c336712930453cb7abd5bef7
Erik Unger <erik@erikunger.com>
2025-10-17 15:38:58 +0200

    Remove nolint directive and add #nosec comment for file permissions in rewriteFile function

--- rewrite.go
+++ rewrite.go
@@ -228,8 +228,8 @@ func rewriteFile(fset *token.FileSet, pkg *ast.Package, filePath string, verbose
 		_, err = resultOut.Write(rewritten)
 		return err
 	}
-	//nolint:gosec // G306: File permissions 0644 are appropriate for generated source files
-	return os.WriteFile(filePath, rewritten, 0644)
+	// File permissions 0644 are appropriate for generated source files
+	return os.WriteFile(filePath, rewritten, 0644) //#nosec G306
 }
 
 func filterOutTests(info os.FileInfo) bool {
